647eb0e1110de1eb8882f54dfdf4e550c9e07829
Manikandan-RR <rajmani1985py@gmail.com>
2025-08-05 16:20:55 +0530

    Name Updated

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -36,7 +36,7 @@ export const MovieList = ({ title, apiPath }) => {
                     (
                         <div className="bg-body-tertiary p-5 border mb-5">
                             <h3 className="text-primary">Welcome to MOVIEINFOS</h3>
-                            <p className="lead"><span style={ownStyle}>Hi, This is Manikandan Rajendran.</span> Discover Movies you&apos;ll love with personalized suggestions, curated collections, and quick searches - your guide to finding great films</p>
+                            <p className="lead"><span style={ownStyle}>Hi, This is Manikandan Rajendran Renganayaki.</span> Discover Movies you&apos;ll love with personalized suggestions, curated collections, and quick searches - your guide to finding great films</p>
                             <button className="btn btn-primary" onClick={() => navigator("/movies/upcoming")}>Explore Now</button>
                         </div>
                     )
